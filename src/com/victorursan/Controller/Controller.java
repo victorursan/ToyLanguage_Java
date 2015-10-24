a98ac4c0e2134762baa32f6f1342b71eca7e7d5c
Victor Ursan <victor.ursan@gmail.com>
2015-10-24 20:32:44 +0300

    added the Repository

---
+++ src/com/victorursan/Controller/Controller.java
@@ -0,0 +1,7 @@
+package com.victorursan.Controller;
+
+/**
+ * Created by victor on 10/24/15.
+ */
+public class Controller {
+}
--- src/com/victorursan/Repository/Repository.java
+++ src/com/victorursan/Repository/Repository.java
@@ -1,7 +1,18 @@
 package com.victorursan.Repository;
 
+import com.victorursan.Models.ProgramState.PrgState;
+
 /**
  * Created by victor on 10/12/15.
  */
 public class Repository {
+    PrgState programStates[];
+
+    public Repository(PrgState[] programStates) {
+        this.programStates = programStates;
+    }
+
+    public PrgState getCrtProgram() {
+        return programStates[0];
+    }
 }
